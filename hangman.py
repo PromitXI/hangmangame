a4e52b56bd518d5ba05ab65980f47422270a99b3
Promit <promit.xi@gmail.com>
2023-01-26 12:35:00 +0530

    Code Completed, Final; Version1.0: Text Color Not Working

--- hangman.py
+++ hangman.py
@@ -1,4 +1,20 @@
 import random
+from termcolor import colored
+
+print(colored('''
+      ___           ___           ___           ___           ___           ___           ___     
+     /\  \         /\  \         /\  \         /\__\         /\  \         /\  \         /\  \    
+     \:\  \       /::\  \        \:\  \       /:/ _/_       |::\  \       /::\  \        \:\  \   
+      \:\  \     /:/\:\  \        \:\  \     /:/ /\  \      |:|:\  \     /:/\:\  \        \:\  \  
+  ___ /::\  \   /:/ /::\  \   _____\:\  \   /:/ /::\  \   __|:|\:\  \   /:/ /::\  \   _____\:\  \ 
+ /\  /:/\:\__\ /:/_/:/\:\__\ /::::::::\__\ /:/__\/\:\__\ /::::|_\:\__\ /:/_/:/\:\__\ /::::::::\__\
+ \:\/:/  \/__/ \:\/:/  \/__/ \:\~~\~~\/__/ \:\  \ /:/  / \:\~~\  \/__/ \:\/:/  \/__/ \:\~~\~~\/__/
+  \::/__/       \::/__/       \:\  \        \:\  /:/  /   \:\  \        \::/__/       \:\  \      
+   \:\  \        \:\  \        \:\  \        \:\/:/  /     \:\  \        \:\  \        \:\  \     
+    \:\__\        \:\__\        \:\__\        \::/  /       \:\__\        \:\__\        \:\__\    
+     \/__/         \/__/         \/__/         \/__/         \/__/         \/__/         \/__/    
+''',"green"))
+
 words = ["dictionary", "refrigerator", "javascript", "beautiful", "excellent", "programming", "difficult", "impossible",
          "champion", "amazing", "extraordinary", "wonderful", "fantastic", "marvelous", "spectacular", "magnificent",
          "splendid", "remarkable", "superb", "delightful", "terrific", "glorious", "impressive", "stupendous",
@@ -57,11 +73,36 @@ HANGMANPICS = ['''
  / \  |
       |
 =========''']
-magic_word= random.choice(words)
-magic_word=magic_word.split()
-game_set=["_" for i in range(len(magic_word))]
-gameon=True
+key = random.choice(words)
+magic_word = list(key)
+
+game_set = ["_" for i in range(len(magic_word))]
+print(game_set)
+gameon = True
+count = 0
 while gameon:
+    if count == 7:
+        print("Game Over")
+        print(f"The Word was {key}")
+        break
 
+    letter = input("Guess a letter to play:: ")
+    if letter in magic_word:
+        if letter in game_set:
+            print(HANGMANPICS[count])
+            count += 1
 
-
+        for i in magic_word:
+            if i == letter:
+                index = magic_word.index(i)
+                game_set[index]=letter
+                print(game_set)
+        if "_" not in game_set:
+            print("Congratulations! You have won the game.")
+            print(f"The word was {key}")
+            gameon = False
+        else:
+            print("".join(game_set))
+    else:
+        print(HANGMANPICS[count])
+        count += 1
